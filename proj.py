da28436d06fe194f2f1a8d48c360c947a436ee7a
rdz <rdszhao@gmail.com>
2021-05-18 13:48:47 -0700

    added hesitancy plot + data

--- proj.py
+++ proj.py
@@ -6,8 +6,7 @@ import requests
 from vega_datasets import data
 # %%
 us_counties = alt.topo_feature(data.us_10m.url, 'counties')
-counties_src = 'https://covid.cdc.gov/covid-data-tracker/COVIDData/getAjaxData?id=vaccination_county_condensed_data'
-resp = requests.get(counties_src)
+resp = requests.get('https://covid.cdc.gov/covid-data-tracker/COVIDData/getAjaxData?id=vaccination_county_condensed_data')
 raw = json.loads(resp.text)
 counties = pd.DataFrame(raw['vaccination_county_condensed_data']).fillna(-1)
 counties.columns = counties.columns.str.lower()
@@ -17,8 +16,10 @@ counties['sfips'] = pd.to_numeric(counties['fips'].str[:2], errors='coerce')
 counties['fips'] = pd.to_numeric(counties['fips'], errors='coerce')
 state_map = counties[['statename', 'sfips']].drop_duplicates().dropna()
 # %%
-tx_counties = 'https://dshs.texas.gov/immunize/covid19/COVID-19-Vaccine-Data-by-County.xls'
-tx = pd.read_excel(tx_counties, sheet_name=1, engine='openpyxl').dropna(axis=1, how='all').dropna(axis=0, how='all')[3:].rename(columns={'County Name': 'county'})
+tx = pd.read_excel(
+    'https://dshs.texas.gov/immunize/covid19/COVID-19-Vaccine-Data-by-County.xls',
+    sheet_name=1, engine='openpyxl'
+).dropna(axis=1, how='all').dropna(axis=0, how='all')[3:].rename(columns={'County Name': 'county'})
 pops = ['Population\n12+', 'Population, 16+', 'Population, 65+']
 tx['population'] = tx[pops].sum(axis=1)
 tx = tx[tx['population'] > 0]
@@ -29,7 +30,7 @@ counties['pct+1'] = counties['pct+1'].fillna(0)
 counties['pct'] = counties['pct'] + counties['pct+1']
 # %%
 us_states = alt.topo_feature(data.us_10m.url, 'states')
-states = pd.read_csv('data/owid_vaccinations.csv').fillna(method='ffill')
+states = pd.read_csv('https://github.com/owid/covid-19-data/blob/master/public/data/vaccinations/us_state_vaccinations.csv?raw=true').fillna(method='ffill')
 states['dt'] = pd.to_datetime(states['date'])
 states = states[states['dt'] > '01/08/2021']
 states['pct'] = states['people_vaccinated_per_hundred']
@@ -43,7 +44,11 @@ states.columns = states.columns.astype(str)
 columns = states.columns.to_list()
 states = states.reset_index()
 # %%
-demos = pd.read_csv('data/demographics.csv', skiprows=5).rename(columns={'Demographic Group': 'group', 'Percent of group with at least one dose': 'pct'}).set_index('Date')[['group', 'pct']]
+resp = requests.get('https://covid.cdc.gov/covid-data-tracker/COVIDData/getAjaxData?id=vaccination_demographic_trends_data')
+raw = json.loads(resp.text)
+demos = pd.DataFrame(raw['vaccination_demographic_trends_data'])
+demos.columns = demos.columns.str.lower()
+demos = demos.rename(columns={'demographic_category': 'group', 'administered_dose1_pct_agegroup': 'pct'}).set_index('date')[['group', 'pct']]
 demos['group'] = demos['group'].str.lower()
 demos = demos[~demos.group.str.contains('known')].reset_index()
 demos.columns = demos.columns.str.lower()
@@ -57,6 +62,15 @@ sex['week'] = sex['dt'].dt.isocalendar().week
 eth['dt'] = pd.to_datetime(eth.date)
 eth = eth[eth['dt'] > '01/08/2021']
 eth['week'] = eth['dt'].dt.isocalendar().week
+# %%
+relevant = ['FIPS Code', 'County Name', 'State', 'Estimated hesitant', 'Estimated hesitant or unsure', 'Estimated strongly hesitant']
+hes = pd.read_csv('https://data.cdc.gov/api/views/q9mh-h2tw/rows.csv', usecols=relevant).rename(columns={'County Name': 'county', 'FIPS Code': 'fips', 'State': 'statename'})
+hes.columns = hes.columns.str.lower()
+hes['pct'] = (hes[hes.columns[hes.columns.str.contains('hesitant')]].sum(axis=1) * 100).round(decimals=1)
+hes['statename'] = hes['statename'].str.title()
+hes_county = hes[['fips', 'county', 'statename', 'pct']]
+hes_states = hes_county.merge(state_map, on='statename', how='left').dropna()
+hes_states = hes_states.groupby(['statename', 'sfips']).mean()['pct'].reset_index()
 # %%
 select_week = alt.selection_single(
     name='week', fields=['week'], init={'week': 2},
@@ -118,7 +132,6 @@ c1b = alt.Chart(us_counties).mark_geoshape(
     height=400
 )
 # %%
-# %%
 c2 = alt.Chart(states_raw).mark_line().encode(
     x='week:N',
     y='pct:Q',
@@ -157,7 +170,46 @@ c3 = alt.hconcat(
     color='independent'
 )
 # %%
-C = ((c1a + c1b) | c3) & c2
+C1 = ((c1a + c1b) | c3) & c2
 # %%
-C
+c4a = alt.Chart(us_states).mark_geoshape(
+    stroke='black',
+    strokeWidth=0.05
+).project(
+    type='albersUsa'
+).transform_lookup(
+    lookup='id',
+    from_=alt.LookupData(hes_states, 'sfips', ['statename', 'pct'])
+).encode(
+    color=alt.Color('pct:Q', scale=alt.Scale(scheme='yellowgreenblue', domain=(0, 100))),
+    tooltip=['pct:Q', 'statename:N']
+).properties(
+    width=700,
+    height=400
+)
+# %%
+c4b = alt.Chart(us_counties).mark_geoshape(
+    stroke='black',
+    strokeWidth=0.1,
+).project(
+    type='albersUsa'
+).transform_lookup(
+    lookup='id',
+    from_=alt.LookupData(hes_county, 'fips', ['statename', 'county', 'pct'])
+).encode(
+    color=alt.Color('pct:Q', scale=alt.Scale(scheme='yellowgreenblue', domain=(0, 100))),
+    opacity=alt.condition(click, alt.value(1), alt.value(0)),
+    tooltip=['county:N', 'pct:N']
+).add_selection(
+    click
+).properties(
+    width=700,
+    height=400
+)
+# %%
+C2 = c4a + c4b
+# %%
+C1
+# %%
+C2
 # %%
